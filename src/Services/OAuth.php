308b710cb0f8f70654b04ec9661e9a44fa2a354a
MrGoodBytes <bytes@goodbytes.live>
2021-03-05 12:45:59 -0600

    Cache permissions and normalize

--- src/Services/OAuth.php
+++ src/Services/OAuth.php
@@ -7,9 +7,9 @@ namespace Bytes\DiscordBundle\Services;
 use Bytes\DiscordResponseBundle\Enums\OAuthPrompts;
 use Bytes\DiscordResponseBundle\Enums\OAuthScopes;
 use Bytes\DiscordResponseBundle\Enums\Permissions;
+use Illuminate\Support\Arr;
 use InvalidArgumentException;
 use LogicException;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -51,25 +51,37 @@ class OAuth
     private $security;
 
     /**
+     * @var array = ['bot' => ['redirects' => ['method' => '', 'route_name' => '', 'url' => ''], 'permissions' => ['add' => [0x0], 'remove' => 0x0]]]
+     */
+    private array $config;
+
+    /**
+     * Cached normalized permissions list
+     * @var array
+     */
+    private $permissions = [];
+
+    /**
      * OAuth constructor.
      * @param Security $security
      * @param UrlGeneratorInterface|null $urlGenerator
      * @param string $discordClientId
-     * @param array $redirects = ['bot' => ['method' => '', 'route_name' => '', 'url' => ''], 'user' => ['method' => '', 'route_name' => '', 'url' => ''], 'slash' => ['method' => '', 'route_name' => '', 'url' => ''], 'login' => ['method' => '', 'route_name' => '', 'url' => '']]
+     * @param array $config
      * @param bool $user
      */
-    public function __construct(Security $security, ?UrlGeneratorInterface $urlGenerator, string $discordClientId, array $redirects, bool $user)
+    public function __construct(Security $security, ?UrlGeneratorInterface $urlGenerator, string $discordClientId, array $config, bool $user)
     {
         $this->discordClientId = $discordClientId;
 
-        $this->userOAuthRedirect = $this->setupRedirect($redirects['user'], $urlGenerator);
-        $this->botOAuthRedirect = $this->setupRedirect($redirects['bot'], $urlGenerator);
-        $this->loginOAuthRedirect = $this->setupRedirect($redirects['login'], $urlGenerator);
-        $this->slashOAuthRedirect = $this->setupRedirect($redirects['slash'], $urlGenerator);
+        $this->userOAuthRedirect = $this->setupRedirect($config['user']['redirects'], $urlGenerator);
+        $this->botOAuthRedirect = $this->setupRedirect($config['bot']['redirects'], $urlGenerator);
+        $this->loginOAuthRedirect = $this->setupRedirect($config['login']['redirects'], $urlGenerator);
+        $this->slashOAuthRedirect = $this->setupRedirect($config['slash']['redirects'], $urlGenerator);
 
         if ($user) {
             $this->security = $security;
         }
+        $this->config = $config;
     }
 
     /**
@@ -115,7 +127,8 @@ class OAuth
             ],
             $this->getBotOAuthRedirect(),
             OAuthScopes::getBotScopes(),
-            $state ?? $this->getState('botRedirect'),
+            $state,
+            'bot',
             'code',
             $guildId,
             !empty($guildId)
@@ -126,7 +139,7 @@ class OAuth
      * @param array $permissions = Permissions::all()
      * @param string $redirect
      * @param array $scopes = OAuthScopes::all()
-     * @param string $state
+     * @param string|null $state
      * @param string $responseType = ['code']
      * @param string|null $guildId
      * @param bool|null $disableGuildSelect
@@ -134,13 +147,19 @@ class OAuth
      *
      * @return string
      */
-    public function getAuthorizationCodeGrantURL(array $permissions, string $redirect, array $scopes, string $state, string $responseType = 'code', ?string $guildId = null, ?bool $disableGuildSelect = null, $prompt = null)
+    public function getAuthorizationCodeGrantURL(array $permissions, string $redirect, array $scopes, ?string $state, string $endpoint, string $responseType = 'code', ?string $guildId = null, ?bool $disableGuildSelect = null, $prompt = null)
     {
         if (empty($prompt)) {
             $prompt = OAuthPrompts::consent();
         }
         if (is_string($prompt)) {
             $prompt = new OAuthPrompts($prompt);
+        }
+        if (array_key_exists($endpoint, $this->permissions)) {
+            $permissions = $this->permissions[$endpoint];
+        } else {
+            $permissions = $this->normalizePermissions($permissions, $endpoint);
+            $this->permissions[$endpoint] = $permissions;
         }
         $query = [
             'client_id' => $this->discordClientId,
@@ -161,11 +180,118 @@ class OAuth
     }
 
     /**
+     * Takes the default permissions list and adds/removes any permissions coming from the config
+     * @param array $permissions
+     * @param string $endpoint
+     * @return array
+     */
+    protected function normalizePermissions(array $permissions, string $endpoint)
+    {
+        if (array_key_exists('add', $this->config[$endpoint]['permissions'])) {
+            $add = $this->config[$endpoint]['permissions']['add'];
+            array_walk($add, array($this, 'hydratePermissions'));
+            $permissions = array_merge($permissions, $add);
+        }
+
+        if (array_key_exists('remove', $this->config[$endpoint]['permissions'])) {
+            $remove = $this->config[$endpoint]['permissions']['remove'];
+            array_walk($remove, array($this, 'hydratePermissions'));
+
+            $permissions = Arr::where($permissions, function ($value, $key) use ($remove) {
+                return !in_array($value, $remove);
+            });
+        }
+
+        return $permissions;
+    }
+
+    /**
      * @return string
      */
     public function getBotOAuthRedirect(): string
     {
         return $this->botOAuthRedirect;
+    }
+
+    /**
+     * @param string|null $guildId
+     * @param string|null $state
+     * @return string
+     */
+    public function getSlashAuthorizationUrl(string $guildId = null, ?string $state = null): string
+    {
+        return $this->getAuthorizationCodeGrantURL(
+            [],
+            $this->getSlashOAuthRedirect(),
+            OAuthScopes::getSlashScopes(),
+            $state,
+            'slash',
+            'code',
+            $guildId,
+            !empty($guildId)
+        );
+    }
+
+    /**
+     * @return string
+     */
+    public function getSlashOAuthRedirect(): string
+    {
+        return $this->slashOAuthRedirect;
+    }
+
+    /**
+     * @param string|null $state
+     * @return string
+     */
+    public function getUserAuthorizationUrl(?string $state = null): string
+    {
+        return $this->getAuthorizationCodeGrantURL(
+            [],
+            $this->getUserOAuthRedirect(),
+            OAuthScopes::getUserScopes(),
+            $state,
+            'user');
+    }
+
+    /**
+     * @return string
+     */
+    public function getUserOAuthRedirect(): string
+    {
+        return $this->userOAuthRedirect;
+    }
+
+    /**
+     * @param string|null $state
+     * @return string
+     */
+    public function getOAuthLoginUrl(?string $state = null): string
+    {
+        return $this->getAuthorizationCodeGrantURL(
+            [],
+            $this->getLoginOAuthRedirect(),
+            [
+                OAuthScopes::IDENTIFY(),
+                OAuthScopes::CONNECTIONS(),
+                OAuthScopes::GUILDS(),
+            ],
+            $state,
+            'login',
+            'code', null, null, OAuthPrompts::none());
+    }
+
+    /**
+     * @return string
+     */
+    public function getLoginOAuthRedirect(): string
+    {
+        return $this->loginOAuthRedirect;
+    }
+
+    protected function hydratePermissions(&$item1, $key)
+    {
+        $item1 = new Permissions($item1);
     }
 
     /**
@@ -213,77 +339,5 @@ class OAuth
         }
 
         return $user;
-    }
-
-    /**
-     * @param string|null $guildId
-     * @param string|null $state
-     * @return string
-     */
-    public function getSlashAuthorizationUrl(string $guildId = null, ?string $state = null): string
-    {
-        return $this->getAuthorizationCodeGrantURL(
-            [],
-            $this->getSlashOAuthRedirect(),
-            OAuthScopes::getSlashScopes(),
-            $state ?? $this->getState('slashRedirect'),
-            'code',
-            $guildId,
-            !empty($guildId)
-        );
-    }
-
-    /**
-     * @return string
-     */
-    public function getSlashOAuthRedirect(): string
-    {
-        return $this->slashOAuthRedirect;
-    }
-
-    /**
-     * @param string|null $state
-     * @return string
-     */
-    public function getUserAuthorizationUrl(?string $state = null): string
-    {
-        return $this->getAuthorizationCodeGrantURL(
-            [],
-            $this->getUserOAuthRedirect(),
-            OAuthScopes::getUserScopes(),
-            $state ?? $this->getState('userRedirect'));
-    }
-
-    /**
-     * @return string
-     */
-    public function getUserOAuthRedirect(): string
-    {
-        return $this->userOAuthRedirect;
-    }
-
-    /**
-     * @param string|null $state
-     * @return string
-     */
-    public function getOAuthLoginUrl(?string $state = null): string
-    {
-        return $this->getAuthorizationCodeGrantURL(
-            [],
-            $this->getLoginOAuthRedirect(),
-            [
-                OAuthScopes::IDENTIFY(),
-                OAuthScopes::CONNECTIONS(),
-                OAuthScopes::GUILDS(),
-            ],
-            $state ?? $this->getState('routeOAuthLogin'), 'code', null, null, OAuthPrompts::none());
-    }
-
-    /**
-     * @return string
-     */
-    public function getLoginOAuthRedirect(): string
-    {
-        return $this->loginOAuthRedirect;
     }
 }
