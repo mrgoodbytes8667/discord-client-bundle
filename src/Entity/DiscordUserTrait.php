8f86fb2c9fef30362e999d980cd24adba18b70bc
MrGoodBytes <bytes@goodbytes.live>
2021-05-26 16:48:26 -0500

    Make discord_id be unique

--- src/Entity/DiscordUserTrait.php
+++ src/Entity/DiscordUserTrait.php
@@ -14,7 +14,7 @@ trait DiscordUserTrait
 {
     /**
      * @var string|null
-     * @ORM\Column(type="string", length=255, nullable=true)
+     * @ORM\Column(type="string", length=255, nullable=true, unique=true)
      */
     private $discord_id;
 
