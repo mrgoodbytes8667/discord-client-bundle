985aaed1533bdba3adb7b103f5b5cdb945064d8b
MrGoodBytes <bytes@goodbytes.live>
2021-06-01 10:20:31 -0400

    Fix for test

--- Tests/Kernel.php
+++ Tests/Kernel.php
@@ -15,6 +15,7 @@ use Symfony\Component\HttpKernel\Bundle\BundleInterface;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
 
 /**
  * Class Kernel
@@ -90,6 +91,7 @@ class Kernel extends BaseKernel
         $loader->load(function (ContainerBuilder $container) {
             $container->register('security.helper', Security::class);
             $container->register('router.default', UrlGeneratorInterface::class);
+            $container->register('security.csrf.token_manager', CsrfTokenManagerInterface::class);
 
             foreach ($this->classes as $class) {
                 if(is_array($class)) {
