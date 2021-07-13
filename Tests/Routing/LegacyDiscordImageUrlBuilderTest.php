7b18c1eef3aecaaaade43a956f4f05fded19b1be
MrGoodBytes <bytes@goodbytes.live>
2021-07-13 09:09:22 -0500

    Deprecate "Bytes\DiscordClientBundle\Routing\DiscordImageUrlBuilder", use "Bytes\DiscordResponseBundle\Routing\DiscordImageUrlBuilder" instead

--- Tests/Routing/LegacyDiscordImageUrlBuilderTest.php
+++ Tests/Routing/LegacyDiscordImageUrlBuilderTest.php
@@ -5,15 +5,17 @@ namespace Bytes\DiscordClientBundle\Tests\Routing;
 use Bytes\Common\Faker\Discord\TestDiscordFakerTrait;
 use Bytes\DiscordClientBundle\Routing\DiscordImageUrlBuilder;
 use Bytes\DiscordResponseBundle\Objects\User;
+use Generator;
 use PHPUnit\Framework\TestCase;
+use Symfony\Bridge\PhpUnit\ExpectDeprecationTrait;
 
 /**
- * Class DiscordImageUrlBuilderTest
+ * Class LegacyDiscordImageUrlBuilderTest
  * @package Bytes\DiscordClientBundle\Tests\Routing
  */
-class DiscordImageUrlBuilderTest extends TestCase
+class LegacyDiscordImageUrlBuilderTest extends TestCase
 {
-    use TestDiscordFakerTrait;
+    use TestDiscordFakerTrait, ExpectDeprecationTrait;
 
     /**
      * @dataProvider provideHashes
@@ -22,6 +24,7 @@ class DiscordImageUrlBuilderTest extends TestCase
      */
     public function testGetAvatarUrl($hash, $gif)
     {
+        $this->expectDeprecation('Since mrgoodbytes8667/discord-client-bundle 0.1.3: This "%s" method is deprecated');
         $userId = $this->faker->userId();
         $user = new User();
         $user->setId($userId)
@@ -32,7 +35,7 @@ class DiscordImageUrlBuilderTest extends TestCase
     }
 
     /**
-     * @return \Generator
+     * @return Generator
      */
     public function provideHashes()
     {
