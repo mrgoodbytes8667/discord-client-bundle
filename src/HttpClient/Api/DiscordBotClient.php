mrgoodbytes8667/discord-client-bundle

commit 315a0fd9db40059946033b0d37dc44f5c468d886

Force the content type to be application/json on all api requests

src/HttpClient/Api/DiscordBotClient.php
@@ -75,6 +75,7 @@ class DiscordBotClient extends DiscordClient
             DiscordClientEndpoints::SCOPE_API => [
                 'headers' => [
                     'Authorization' => 'Bot ' . $botToken,
+                    'Content-Type' => 'application/json',
                 ],
             ],
         ], $defaultOptionsByRegexp);

commit 5b5a8cb8564a4feb71f5e2f435de5764e9729d47

Fix bug introduced by 315a0fd9db40059946033b0d37dc44f5c468d886, set the content type individually on endpoints that need it

src/HttpClient/Api/DiscordBotClient.php
@@ -85,7 +85,6 @@ class DiscordBotClient extends DiscordClient
             DiscordClientEndpoints::SCOPE_API => [
                 'headers' => [
                     'Authorization' => 'Bot ' . $botToken,
-                    'Content-Type' => 'application/json',
                 ],
             ],
         ], $defaultOptionsByRegexp);
@@ -765,6 +764,9 @@ class DiscordBotClient extends DiscordClient
         ]);
 
         return $this->request($urlParts, caller: $caller, type: Message::class, options: [
+            'headers' => [
+                'Content-Type' => 'application/json',
+            ],
             'body' => $body
         ], method: $method, onSuccessCallable: $onSuccessCallable);
     }

src/HttpClient/Api/DiscordClient.php
@@ -276,10 +276,16 @@ class DiscordClient extends AbstractApiClient implements SerializerAwareInterfac
 
         if ($method === HttpMethods::patch() && !empty($messageId)) {
             return $this->request($urlParts, caller: $caller, type: Message::class, options: [
+                'headers' => [
+                    'Content-Type' => 'application/json',
+                ],
                 'body' => $body
             ], method: $method);
         } elseif ($wait) {
             return $this->request($urlParts, caller: $caller, type: Message::class, options: [
+                'headers' => [
+                    'Content-Type' => 'application/json',
+                ],
                 'body' => $body,
                 'query' => [
                     'wait' => $wait
@@ -287,6 +293,9 @@ class DiscordClient extends AbstractApiClient implements SerializerAwareInterfac
             ], method: $method);
         } else {
             return $this->request($urlParts, caller: $caller, options: [
+                'headers' => [
+                    'Content-Type' => 'application/json',
+                ],
                 'body' => $body,
                 'query' => [
                     'wait' => $wait