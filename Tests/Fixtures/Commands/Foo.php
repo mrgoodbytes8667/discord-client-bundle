5004d214178d7c21527173c94e102a5a84135929
MrGoodBytes <bytes@goodbytes.live>
2021-09-15 11:45:15 -0500

    Added allowEveryoneRole() and getPermissionsForRoles() to SlashCommandInterface [breaking change]

--- Tests/Fixtures/Commands/Foo.php
+++ Tests/Fixtures/Commands/Foo.php
@@ -5,6 +5,7 @@ namespace Bytes\DiscordClientBundle\Tests\Fixtures\Commands;
 
 use Bytes\DiscordClientBundle\Slash\SlashCommandInterface;
 use Bytes\DiscordResponseBundle\Enums\ApplicationCommandOptionType as ACOT;
+use Bytes\DiscordResponseBundle\Enums\Permissions;
 use Bytes\DiscordResponseBundle\Objects\Application\Command\ChatInputCommand;
 use Bytes\DiscordResponseBundle\Objects\Slash\ApplicationCommand;
 use Bytes\DiscordResponseBundle\Objects\Slash\ApplicationCommandOption as Option;
@@ -36,4 +37,21 @@ class Foo implements SlashCommandInterface
         return 'foo';
     }
 
+    /**
+     * @return bool False will prevent the permissions command from displaying the @everyone role.
+     */
+    public static function allowEveryoneRole(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Returns an array of Discord Permission enums that will be used by the add command to build a list of roles to get
+     * access to run the command. Any role that has any permission in this list will get access.
+     * @return Permissions[]
+     */
+    public static function getPermissionsForRoles(): array
+    {
+        return [];
+    }
 }
