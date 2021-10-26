7de4fc262111fbde4a4654350f48934a91f67b4c
MrGoodBytes <bytes@goodbytes.live>
2021-10-26 14:34:54 -0500

    Replace faker with Discord faker

--- Tests/Request/TestParamConverterCase.php
+++ Tests/Request/TestParamConverterCase.php
@@ -4,10 +4,8 @@
 namespace Bytes\DiscordClientBundle\Tests\Request;
 
 
-use Bytes\Common\Faker\Providers\MiscProvider;
+use Bytes\Common\Faker\Discord\TestDiscordFakerTrait;
 use Bytes\Tests\Common\TestParamConverterTrait;
-use Faker\Factory;
-use Faker\Generator;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -16,32 +14,5 @@ use PHPUnit\Framework\TestCase;
  */
 class TestParamConverterCase extends TestCase
 {
-    use TestParamConverterTrait;
-
-    /**
-     * @var MiscProvider|Generator
-     */
-    protected $faker;
-
-    /**
-     * @return MiscProvider|Generator
-     * @before
-     */
-    public function setupFaker()
-    {
-        if (is_null($this->faker)) {
-            $faker = Factory::create();
-            $faker->addProvider(new MiscProvider($faker));
-            $this->faker = $faker;
-        }
-        return $this->faker;
-    }
-
-    /**
-     * @after
-     */
-    protected function tearDownFaker(): void
-    {
-        $this->faker = null;
-    }
+    use TestParamConverterTrait, TestDiscordFakerTrait;
 }
